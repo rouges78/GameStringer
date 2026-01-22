f6c01bfd7d9a48922748be4a25f75103350e02a1
rouges78 <rouges78@gmail.com>
2026-01-22 17:07:22 +0100

    fix: Rimuovo MainLayout duplicato da manga-translator page

--- app/manga-translator/page.tsx
+++ app/manga-translator/page.tsx
@@ -2,12 +2,11 @@
 
 import React from 'react';
 import { MangaTranslator } from '@/components/tools/manga-translator';
-import { MainLayout } from '@/components/layout/main-layout';
 
 export default function MangaTranslatorPage() {
   return (
-    <MainLayout>
+    <div className="p-4 overflow-y-auto h-full">
       <MangaTranslator />
-    </MainLayout>
+    </div>
   );
 }
